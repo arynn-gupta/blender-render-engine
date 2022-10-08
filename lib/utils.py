5ecf476a58c470f4ff73f0c3e0add4f0553f314e
Aryan Gupta <arynn.gupta@gmail.com>
2022-10-08 19:55:50 +0530

    new fix

--- lib/utils.py
+++ lib/utils.py
@@ -112,15 +112,16 @@ def make_zip(download_file_path, ele):
     output_file_name = file_name + ".zip"
     if(os.path.isfile(file_path)):
         shutil.make_archive(file_name, 'zip', st.session_state["current_path"], download_file_path)
-    else:
+    elif len(os.listdir(file_path)) != 0:
         shutil.make_archive(file_name, 'zip', file_path)
-    file = open(f"{output_file_name}", "rb")
-    ele.download_button(
-            label="Download Zip",
-            data=file,
-            file_name=f"{output_file_name}",
-            mime="application/zip",
-            on_click =clear_download,
-            args=(output_file_name, ele)
-        )
+    if(os.path.exists(output_file_name)):
+      file = open(f"{output_file_name}", "rb")
+      ele.download_button(
+              label="Download Zip",
+              data=file,
+              file_name=f"{output_file_name}",
+              mime="application/zip",
+              on_click =clear_download,
+              args=(output_file_name, ele)
+          )
     file.close()
